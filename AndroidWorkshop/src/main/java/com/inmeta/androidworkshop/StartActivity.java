f4274b9620c4187ca4f405a2c7b1bbdacd8a2bf6
glennbech <glenn.bech@gmail.com>
2013-11-19 14:10:39 +0100

    project

---
+++ AndroidWorkshop/src/main/java/com/inmeta/androidworkshop/StartActivity.java
@@ -0,0 +1,66 @@
+package com.inmeta.androidworkshop;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+
+/**
+ * This is this applications start Activity. The class name is referenced from Androidmanifest.xml
+ *
+ * @author Glenn Bech
+ */
+public class StartActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        boolean noSavedState = savedInstanceState == null;
+        if (noSavedState) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    /**
+     * Override this method to create a menu, or action bar items for Android 3.0 +
+     *
+     * @param menu the menu resource to "inflate"
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.start, menu);
+        return true;
+    }
+
+    /**
+     * This method is called when a menu item is selected.
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
